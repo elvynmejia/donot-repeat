e696f2a5187152c3643fb7c26ef0b651b6f17612
Elvyn Mejia <elvynmejia@gmail.com>
2021-03-11 18:02:46 -0800

    Use callback to get data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 
-
-import logo from './logo.svg';
 import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 const API_URL = 'http://localhost:9292/stats';
 
@@ -35,20 +12,22 @@ const App = (props) => {
     revenue_by_product: {},
   });
 
+  const fetchData = useCallback(async () => {
+    // handle errors try/catch
+    const response = await axios.get(API_URL);
+    setData(response.data);
+  }, [])
 
-  useEffect(async () => {
-    const result = await axios(
-      API_URL,
-    );
 
-    setData(result.data);
-  });
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
 
   return (
     <div>
       <h5>Welcome to Repeat</h5>
       <p>See analytics some analytics below</p>
-      <pre>{data}</pre>
+      <pre>{JSON.stringify(data)}</pre>
     </div>
 
   )
